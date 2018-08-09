d1b8d2d2ed212860c1479a044e5471a22a0c8a54
webdesarrollo <webdesarrollo@protonmail.com>
2018-08-09 15:19:18 -0300

    sidemenu active
    with easynav

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -4,24 +4,34 @@
       GENERAL
     </p>
     <ul class="menu-list">
-      <li><a href="">Dashboard</a></li>
+      <li>
+        <a href="{{route('manage.dashboard')}}" class="{{Nav::isRoute('manage.dashboard')}}">
+          Dashboard
+        </a>
+      </li>
     </ul>
 
     <p class="menu-label">
       ADMINISTRATION
     </p>
     <ul class="menu-list">
-      <li><a href="{{route('users.index')}}">Manage Users</a></li>
       <li>
-        <a href="#" class="has-submenu">
+        <a href="{{route('users.index')}}" class="{{Nav::isResource('users')}}">
+          Manage Users
+        </a>
+      </li>
+      <li>
+        <a class="has-submenu {{Nav::hasSegment(['roles', 'permissions'], 2)}}">
           Roles &amp; Permissions
         </a>
         <ul class="submenu">
           <li>
-            <a href="{{route('roles.index')}}">Roles</a>
+            <a href="{{route('roles.index')}}" class="{{Nav::isResource('roles')}}">
+              Roles
+            </a>
           </li>
           <li>
-            <a href="{{route('permissions.index')}}">
+            <a href="{{route('permissions.index')}}" class="{{Nav::isResource('permissions')}}">
               Permissions
             </a>
           </li>
